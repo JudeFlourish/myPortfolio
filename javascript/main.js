9c30b3749feb1f62d31f076a20cb310a66ed7d9d
Jude Flourish <judeflourish01@gmail.com>
2025-04-14 14:08:37 +0100

    Updated Portfolio

--- javascript/main.js
+++ javascript/main.js
@@ -19,3 +19,45 @@ function closeModal(event) {
 }
 
 
+//ANIMATION
+const animatedSections = document.querySelectorAll('.fade-slide');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active'); // <- this makes it re-animate when scrolling back up
+    }
+  });
+}, {
+  threshold: 0.15 // adjust this to your liking
+});
+
+animatedSections.forEach(section => observer.observe(section));
+
+
+
+
+
+
+//show/hide nav menu
+const menu = document.querySelector(".nav_content");
+const menuBtn = document.querySelector(".open_menu_btn");
+const closeBtn = document.querySelector(".close_menu_btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
